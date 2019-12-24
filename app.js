139d17cbf45df2eb6733dfe484ae6c71495265c2
RussellBentley <russell.w.bentley@icloud.com>
2019-12-24 10:05:18 -0800

    Add package.json and workflow stuff. Also format all the things

--- app.js
+++ app.js
@@ -20,12 +20,14 @@ class CalculatorApp {
 
   enter_digit(digit) {
     this.register += String(digit);
-    console.log("Digit entered: " + digit + ", register is now " + this.register);
+    console.log(
+      "Digit entered: " + digit + ", register is now " + this.register
+    );
     this.display_element.display(this.register);
   }
 
   decimal() {
-    this.register += '.'
+    this.register += ".";
     console.log("Decimal entered, register is now " + this.register);
     this.display_element.display(this.register);
   }
@@ -42,7 +44,10 @@ class CalculatorApp {
     // then we will interpret the register as negative
     if (this.current_op != "null" && this.register == "") {
       this.register += "-";
-      console.log("Sub entered with non-null op and empty register, register is now " + this.register);
+      console.log(
+        "Sub entered with non-null op and empty register, register is now " +
+          this.register
+      );
       this.display_element.display(this.register);
     } else {
       this.current_op = "sub";
@@ -76,7 +81,16 @@ class CalculatorApp {
     } else if (this.current_op == "div") {
       new_result = old_result / old_register;
     }
-    console.log("enter, old_result: " + old_result + " old_register: " + old_register + " current_op: " + this.current_op + " new_result: " + new_result);
+    console.log(
+      "enter, old_result: " +
+        old_result +
+        " old_register: " +
+        old_register +
+        " current_op: " +
+        this.current_op +
+        " new_result: " +
+        new_result
+    );
     this.result = new_result;
     this.register = "";
     this.current_op = "null";
@@ -97,7 +111,9 @@ class TextDisplay {
 // Javascript "closures" capture by reference not value
 // soo you have to capture parameters instead?
 function digit_callback(calculator, digit) {
-  return function() { calculator.enter_digit(digit); }
+  return function() {
+    calculator.enter_digit(digit);
+  };
 }
 
 function bindButton(document, id, callback) {
@@ -124,32 +140,50 @@ function bindCalculatorApp(svg_document) {
     var button_name = "button_" + digit;
     bindButton(svg_document, button_name, digit_callback(calculator, digit));
   }
-  bindButton(svg_document, "button_decimal", function() {calculator.decimal();});
-  bindButton(svg_document, "button_add", function() {calculator.add_op();});
-  bindButton(svg_document, "button_sub", function() {calculator.sub_op();});
-  bindButton(svg_document, "button_mul", function() {calculator.mul_op();});
-  bindButton(svg_document, "button_div", function() {calculator.div_op();});
-  bindButton(svg_document, "button_enter", function() {calculator.enter();});
-  bindButton(svg_document, "button_clear", function() {calculator.clear();});
+  bindButton(svg_document, "button_decimal", function() {
+    calculator.decimal();
+  });
+  bindButton(svg_document, "button_add", function() {
+    calculator.add_op();
+  });
+  bindButton(svg_document, "button_sub", function() {
+    calculator.sub_op();
+  });
+  bindButton(svg_document, "button_mul", function() {
+    calculator.mul_op();
+  });
+  bindButton(svg_document, "button_div", function() {
+    calculator.div_op();
+  });
+  bindButton(svg_document, "button_enter", function() {
+    calculator.enter();
+  });
+  bindButton(svg_document, "button_clear", function() {
+    calculator.clear();
+  });
 
   console.log("binding calculator app complete");
 }
 
 function init() {
-   var svg_object = document.getElementById("svg_object");
+  var svg_object = document.getElementById("svg_object");
   if (svg_object == null) {
     console.error("svg_object not found!");
     return;
   }
 
-  svg_object.addEventListener("load",function() {
-    console.log("SVG contentDocument Loaded!");
-    if(svg_object.contentDocument == null) {
-      console.error("contentDocument is still null!");
-    } else {
-      bindCalculatorApp(svg_object.contentDocument);
-    }
-    }, false);
+  svg_object.addEventListener(
+    "load",
+    function() {
+      console.log("SVG contentDocument Loaded!");
+      if (svg_object.contentDocument == null) {
+        console.error("contentDocument is still null!");
+      } else {
+        bindCalculatorApp(svg_object.contentDocument);
+      }
+    },
+    false
+  );
 
   console.log("init done");
 }
